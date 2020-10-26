13c763b93336c79c7c9b781b25a8b1e551a9f9fb
Rafael Mendes <mendesrafa.c@gmail.com>
2020-10-26 17:44:31 -0300

    adding initial files to the project

---
+++ src/styles/styles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const colors = {
+
+}
+
+const GlobalStyle = styled.GlobalStyle`
+
+    *, *::before, *::after {
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: Lato;
+    }
+
+`;
+
