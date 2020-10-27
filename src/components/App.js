2f8abb500d9193ff62f7eee4eafec3075f84b29c
paulovfmarques <marquespaulov@gmail.com>
2020-10-27 10:43:23 -0300

    header layout

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import GlobalStyle from '../styles/styles';
 import { UserProvider } from '../contexts/UserContext';
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
+import TimeLine from '../pages/TimeLine';
 
 const App = () => {
     return (
@@ -11,15 +12,11 @@ const App = () => {
             <GlobalStyle />
             <Router>
                 <Switch>
-                    <Route path='/' exact>
-                        <SignIn />
-                    </Route>
-                    <Route path='/sign-up'>
-                        <SignUp />
-                    </Route>
-                    <Route path='/timeline'>
-                        
-                    </Route>
+                    <Route path='/' exact component={SignIn} />
+                       
+                    <Route path='/sign-up' component={SignUp} />
+                       
+                    <Route path='/timeline' component={TimeLine} />                     
                 </Switch>
             </Router>
         </UserProvider>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,59 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import styled from 'styled-components';
+import { FaChevronDown } from 'react-icons/fa';
+import { colors } from '../styles/styles';
+import UserContext from '../contexts/UserContext'
+
+const Header = () => {
+
+    const { User } = useContext(UserContext);
+
+    const { token, user } = User;
+
+    console.log(user);
+
+    return(
+        <HeaderContainer>
+            <h1>linkr</h1>
+            <div>
+                <DropDownIcon />
+                <img src={user.avatar}/>
+            </div>
+        </HeaderContainer>
+    );
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+    width: 100%;
+    height: 4rem;
+    display: flex;
+    padding: 0 1rem;
+    justify-content: space-between;  
+    align-items: center;
+    background-color: ${colors.bgHeader};
+
+    h1 {
+        font-size: 2.75rem;
+        color: ${colors.secondaryText};
+    }
+
+    & > div {
+        display: flex;
+        align-items: center;
+    }
+
+    img {
+        height: 2.75rem;
+        width: auto;
+        border-radius: 50%;
+        margin-left: 1rem;
+    }
+`;
+
+const DropDownIcon = styled(FaChevronDown)`
+    font-size: 1.5rem;
+    color: ${colors.secondaryText};
+    cursor: pointer;
+`;
